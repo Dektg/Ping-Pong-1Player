bb58d1d140d14e92d87d5be718216772eb11f473
Dektg <56469979+Dektg@users.noreply.github.com>
2020-01-07 20:40:23 +1000

    Работающая программа для одного пользователя

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -1,31 +1,31 @@
-import turtle
-from random import choice, randint
+import turtle#
+from random import choice, randint#
 
-score = 0
+#score = 0
 
-window = turtle.Screen()
-window.title("2d Ping-Pong")
-# Размеры окна на весь монитор
-window.setup(width=1.0, height=1.0)
-# Задний фон
-window.bgcolor("black")
-#window.tracer(2)
+window = turtle.Screen()#
+window.title("2d Ping-Pong")#
+# Размеры окна на весь монитор#
+window.setup(width=1.0, height=1.0)#
+# Задний фон#
+window.bgcolor("black")#
+window.tracer(2)
 
 # Отрисовка поля
-border_rectangle = turtle.Turtle()
-border_rectangle.color("white")
-border_rectangle.speed(5)
-# Спрятал отрисовку курсора
-border_rectangle.hideturtle()
-border_rectangle.up()
-border_rectangle.goto(-500, 300)
-border_rectangle.down()
-border_rectangle.begin_fill()
-border_rectangle.goto(500, 300)
-border_rectangle.goto(500, -300)
-border_rectangle.goto(-500, -300)
-border_rectangle.goto(-500, 300)
-border_rectangle.end_fill()
+border_rectangle = turtle.Turtle()#
+border_rectangle.color("white")#
+border_rectangle.speed(5)#
+# Спрятал отрисовку курсора#
+border_rectangle.hideturtle()#
+border_rectangle.up()#
+border_rectangle.goto(-500, 300)#
+border_rectangle.down()#
+border_rectangle.begin_fill()#
+border_rectangle.goto(500, 300)#
+border_rectangle.goto(500, -300)#
+border_rectangle.goto(-500, -300)#
+border_rectangle.goto(-500, 300)#
+border_rectangle.end_fill()#
 
 #FONT = ("Arial", 44)
 #score = 0
@@ -35,27 +35,29 @@ border_rectangle.end_fill()
 #s1.setposition(-200, 300)
 #s1.write(score, font=FONT)
 
-rocket = turtle.Turtle()
-rocket.color("black")
-rocket.shape("square")
-rocket.shapesize(stretch_len=5, stretch_wid=1)
-# Не оставляет следов
-rocket.penup()
-rocket.goto(-450, 0)
-space_rocket = 80
+rocket = turtle.Turtle()#
+rocket.color("black")#
+rocket.shape("square")#
+rocket.shapesize(stretch_len=5, stretch_wid=1)#
+# Не оставляет следов#
+rocket.penup()#
+rocket.goto(0, -250)#
+
+space_rocket = 80#
+
+
+def move_Right():#
+    x = rocket.xcor() + space_rocket
+    if x > 490:
+        x = 490
+    rocket.setx(x)
+
 
 def move_Left():
-    y = rocket.ycor() + space_rocket
-    if y > 250:
-        y = 250
-    rocket.sety(y)
-
-
-def move_Right():
-    y = rocket.ycor() - space_rocket
-    if y < -250:
-        y = -250
-    rocket.sety(y)
+    x = rocket.xcor() - space_rocket
+    if x < -490:
+        x = -490
+    rocket.setx(x)
 
 
 ball = turtle.Turtle()
@@ -66,8 +68,8 @@ ball.dy = 5
 ball.penup()
 
 window.listen()
-window.onkeypress(move_Left, "Left")
-window.onkeypress(move_Right, "Right")
+window.onkeypress(move_Right, "d")
+window.onkeypress(move_Left, "a")
 
 while True:
     ball.setx(ball.xcor() + ball.dx)
@@ -85,8 +87,8 @@ while True:
     if ball.xcor() <= -490:
         ball.dx = -ball.dx
 
-    if ball.ycor() >= rocket.ycor() - 50 and ball.ycor() <= rocket.ycor() + 50 \
-            and ball.xcor() >= rocket.xcor() - 5 and ball.xcor() <= rocket.xcor() + 5:
-        ball.dx = -ball.dx
+    if ball.ycor() >= rocket.ycor() - 5 and ball.ycor() <= rocket.ycor() + 5 \
+            and ball.xcor() >= rocket.xcor() - 50 and ball.xcor() <= rocket.xcor() + 50:
+        ball.dy = -ball.dy
 
 window.mainloop()
